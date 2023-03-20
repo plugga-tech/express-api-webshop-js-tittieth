7bdd3b7aa998774811a2cd6a73f6aaf83a313a9e
tittieth <114925706+tittieth@users.noreply.github.com>
2023-03-20 21:58:22 +0100

    added product model, updated routes

---
+++ models/product-model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        required: true
+    }
+},
+
+{timestamps: true}
+
+);
+
+module.exports = mongoose.model('product', ProductSchema)

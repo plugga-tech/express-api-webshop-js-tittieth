b95f05ba483e36c331e6cc7a8ae33db2e687c073
tittieth <114925706+tittieth@users.noreply.github.com>
2023-03-23 14:58:17 +0100

    can add product (VG)

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const ProductModel = require("../models/product-model");
+const CategoryModel = require("../models/category-model");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -29,12 +30,42 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
-router.post("/add", async (req, res, next) => {
+router.get("/category/:id", async (req, res, next) => {
   try {
-    const product = new ProductModel(req.body);
-    console.log(product);
-    await product.save();
-    res.status(200).json(product);
+    const findProduct = await ProductModel.find({ category: req.params.id });
+    console.log(findProduct);
+    if (findProduct) {
+      res.status(200).json(findProduct);
+    } else {
+      res.status(404).json("No product found");
+    }
+  } catch (error) {
+    res.status(400).json(error)
+  }
+});
+
+// router.post("/add", async (req, res, next) => {
+//   try {
+//     const product = new ProductModel(req.body);
+//     console.log(product);
+//     await product.save();
+//     res.status(200).json(product);
+//   } catch (error) {
+//     res.status(400).json(error)
+//   }
+// });
+
+router.post("/add", async (req, res, next) => {
+  const { token } = req.body;
+  try {
+    if (token === "1234key1234") {
+      const product = new ProductModel(req.body);
+      console.log(product);
+      await product.save();
+      res.status(200).json(product);
+    } else {
+      res.status(401).json("not authorized to do that")
+    }
   } catch (error) {
     res.status(400).json(error)
   }
